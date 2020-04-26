c1f393079ec1d3858b0633267fa436567cb70828
Kamil Jabłoński <kamil.jablonski@jestemcontent.pl>
2020-04-26 13:50:44 +0200

    Update deadths.py

--- deadths.py
+++ deadths.py
@@ -4,104 +4,81 @@
 
 import pandas as pd
 import numpy as np
-from wwo_hist import retrieve_hist_data
-
-
-#%%
-
-df = pd.read_csv('./LUDN_2286_CTAB_20200422181131.csv', delimiter=';')
-
-
-list_of_columns = list(df.columns)
-list_of_new_columns = ['kod', 'nazwa', '2002', '2003', '2004', '2005', 
-                       '2006', '2007', '2008', '2009', '2010', '2011', '2012', 
-                       '2013', '2014', '2015', '2016', '2017', '2018', '2019']
-
-#%%
-df.columns = list_of_new_columns 
-
-#%%
-
-
-
-df_new = df[df['nazwa'].str.isupper() == False]
-
-df_sum = df_new.sum()
-df_sum = df_sum.drop(['kod', 'nazwa','2019'], axis=0)
-
-df_sum = pd.DataFrame(df_sum)
-columns_sum = ['sum']
-df_sum.columns = columns_sum
-
-#%%
-
-
-
-#%%
-
-# weather data form https://www.worldweatheronline.com/
-
-
-import os
-os.chdir("./")
-
-frequency=24
-start_date = '02-01-2010'
-end_date = '02-01-2019'
-api_key = '3287e0d3bbb24204bef170141202204'
-location_list = ['poland']
-
-hist_weather_data = retrieve_hist_data(api_key,
-                                location_list,
-                                start_date,
-                                end_date,
-                                frequency,
-                                location_label = False,
-                                export_csv = False,
-                                store_df = True)
-
-#%%
-
-poland_weather_data = hist_weather_data[0]
-print(poland_weather_data.head())
-
-
-#%%
-pwd_mean = poland_weather_data.set_index('date_time')
-
-pwd_mean.head()
-
-pwd_mean = pwd_mean.astype({"maxtempC": int, "mintempC": int})
-print(type(pwd_mean.iloc[0,0]))
-pwd_mean_temp = pwd_mean[['maxtempC','mintempC']]
-
-#%%
-pwd_mean_temp = pwd_mean.resample('Y').mean()
-pwd_mean_temp = pwd_mean_temp.reset_index()
-
-
-
 import datetime
-pwd_mean_temp['year'] = pd.DatetimeIndex(pwd_mean_temp['date_time']).year
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-
-df_sum['year'] = df_sum.index
-
-pwd_mean_temp = pwd_mean_temp.set_index('year')
-df_sum = df_sum[['sum','year']]
 
 #%%
 
+#loading data to df's
+
+deaths_data = pd.read_csv('./LUDN_2286_CTAB_20200422181131.csv', delimiter=';')
+weather_data = pd.read_csv('./pwd.csv')
+
+deaths_data.head()
+
+#%%
+weather_data.head()
+
+#%%
+
+# cleaning deadhs data
+
+# executing just general polish data of deadhs
+
+deaths_data = deaths_data[deaths_data['Nazwa']== 'POLSKA']
+
+#%%
+deaths_data = deaths_data.drop(['Nazwa','Kod'], axis=1)
+#%%
+list_of_new_colnames = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+                        2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+deaths_data.columns = list_of_new_colnames
+deaths_data = deaths_data.drop(2019, axis=1)
+
+#%%
+# transposing data frame
+
+deaths_data = deaths_data.T
+
+#%%
+
+weather_data.head()
+weather_data['year'] = pd.DatetimeIndex(weather_data['date_time']).year
+future_index_type = str(type(weather_data.loc[1,'year']))
+
+#%%
+
+weather_data = weather_data.set_index('year')
+
+#%%
+weather_data_mean = weather_data.groupby(level='year').mean()
+
+#%%
+
+full_df = deaths_data.merge(weather_data_mean, left_index=True, right_index=True)
+full_df = full_df.rename(columns={0: 'deaths'})
 
 
+#%%
+
+# correlation
+
+#def get_corrs(df):
+    #col_correlations = df.corr()
+    #col_correlations.loc[:, :] = np.tril(col_correlations, k=-1)
+    #cor_pairs = col_correlations.stack()
+    #return cor_pairs.to_dict()
+
+#corr_df = get_corrs(full_df)
+    
+#%%
+    
+# corr testing
 
 
-
-
-
-
-
-
+    
 
 
 
